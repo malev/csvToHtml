7372cb8fb5412a7dd09b7e3754f32bcbc31fe3bd
malev <marcosvanetta@gmail.com>
2014-05-26 16:57:42 -0500

    Generate basic table

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,20 @@
 //=require underscore/underscore
 //=require jquery/dist/jquery
 //=require papa-parse/jquery.parse
+//=require templates/table
+
+
+$(document).ready(function(){
+  var template = JST['templates/table'];
+
+  $(".convert").on('click', function(event){
+    event.preventDefault();
+    var input = $('textarea').val();
+    var results = esults = $.parse(input, {
+      header: false
+    });
+    $(".output").html(
+      template({rows: results})
+    );
+  });
+});
