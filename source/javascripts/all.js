583ba7086c60a3da44bb00bf23f2b7783abdf208
malev <marcosvanetta@gmail.com>
2014-05-29 02:05:33 -0500

    Add class and id to table

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,18 +3,51 @@
 //=require papa-parse/jquery.parse
 //=require templates/table
 
+var viewHelpers = {
+  table: function(klass, options){
+    if (klass){
+      return '<table class="' + klass + '"">';
+    } else {
+      return '<table>';
+    }
+  },
+  tag: function(name, options){
+    var id = options['id'],
+        klass = options['klass'],
+        output = '<' + name;
+
+    if(id){
+      output += ' id="' + id + '"';
+    }
+    if(klass){
+      output += ' class="' + klass + '"';
+    }
+    output += '>'
+    return output;
+  }
+}
 
 $(document).ready(function(){
   var template = JST['templates/table'];
 
-  $(".convert").on('click', function(event){
-    event.preventDefault();
+  // $(".convert").on('click', function(event){
+    // event.preventDefault();
     var input = $('textarea').val();
     var parsed = $.parse(input, {
       header: true
     });
-    $(".output").html(
-      template({rows: parsed.results.rows, fields: parsed.results.fields})
-    );
-  });
+
+    var data = {
+      rows: parsed.results.rows,
+      fields: parsed.results.fields,
+      tableOptions: {
+        klass: 'table-klass',
+        id: 'table-id'
+      }
+    }
+
+    _.extend(data, viewHelpers);
+
+    $(".output").html(template(data));
+  // });
 });
