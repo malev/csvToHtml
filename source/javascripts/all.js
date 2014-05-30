de6f69fe7bf6f42eead555a26b5b590db2f3ce26
malev <marcosvanetta@gmail.com>
2014-05-29 23:34:25 -0500

    Generate final html

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -11,43 +11,79 @@ var viewHelpers = {
       return '<table>';
     }
   },
-  tag: function(name, options){
-    var id = options['id'],
-        klass = options['klass'],
-        output = '<' + name;
+  theads: function(cells, options){
+    var output = '',
+        self = this;
 
-    if(id){
-      output += ' id="' + id + '"';
-    }
-    if(klass){
-      output += ' class="' + klass + '"';
-    }
-    output += '>'
+    _.each(cells, function(cell, idx){
+      output += self.tag('th', options[idx]);
+      output += cell + '</th>';
+    });
+
     return output;
+  },
+  tableElements: function(name, row, options){
+    var output = '',
+        self = this;
+
+    row = _.values(row);
+
+    _.each(row, function(cell, idx){
+      output += self.tag(name, options[idx]);
+      output += cell + '</' + name + '>';
+    });
+
+    return output;
+  },
+  tag: function(name, options){
+    var output = '<' + name,
+        attributes = this.buildAttributes(options);
+
+    if(attributes.length === 0){
+      output += '>';
+    } else {
+      output += ' '+ attributes + '>';
+    }
+    return output;
+  },
+  buildAttributes: function(options){
+    var output = [];
+    for (var key in options) {
+      if (options.hasOwnProperty(key)){
+        output.push(key + '="' + options[key] + '"');
+      }
+    }
+    return output.join(' ');
   }
 }
+
 
 $(document).ready(function(){
   var template = JST['templates/table'];
 
-  // $(".convert").on('click', function(event){
-    // event.preventDefault();
-    var input = $('textarea').val();
-    var parsed = $.parse(input, {
-      header: true
-    });
+  var input = $('textarea').val();
+  var parsed = $.parse(input, {
+    header: true
+  });
 
-    var data = {
-      rows: parsed.results.rows,
-      fields: parsed.results.fields,
-      tableOptions: {
-        klass: 'table-klass',
-        id: 'table-id'
-      }
+  var data = {
+    rows: parsed.results.rows,
+    fields: parsed.results.fields,
+    tableOptions: {
+      klass: 'table-klass',
+      id: 'table-id'
+    },
+    thOptions: {
+      0: {'class': 'first-klass'},
+      1: {'class': 'second-klass'}
+    },
+    tdOptions: {
+      0: {'data-title': 'Name', 'class': 'center'},
+      1: {'data-title': 'Gender', 'class': 'right'}
     }
+  }
 
-    _.extend(data, viewHelpers);
+  _.extend(data, viewHelpers);
 
-    $(".output").html(template(data));
-  // });
+  $(".output").html(template(data));
 });
