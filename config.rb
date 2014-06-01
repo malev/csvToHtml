43109ed4e5398814da6dad85b6f2fb7728f06231
malev <marcosvanetta@gmail.com>
2014-06-01 13:54:12 -0500

    Add deploy configuration

--- config.rb
+++ config.rb
@@ -1,6 +1,13 @@
-activate :bower
-activate :livereload
-
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+
+activate :bower
+activate :livereload
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host   = "codingnews.info"
+  deploy.path   = "/home/malev/apps/csv.codingnews.info"
+  deploy.user   = "malev"
+  deploy.build_before = true
+end
